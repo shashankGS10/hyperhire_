dd4b65dac4a8c343ebb110f32a60bc44da908150
Shashank G S <gsshashank@gmail.com>
2024-12-02 20:58:53 +0530

    test 6

--- lib/api.ts
+++ lib/api.ts
@@ -37,7 +37,9 @@ function getBaseUrl() {
     return '';
   }
   
-  if (process.env.VERCEL_URL) {
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  } else if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
   
